30b9a4e7600ac4ddbb6c47ea1a39151c4fa150ab
Angeloxzs <70868798+angelomds42@users.noreply.github.com>
2024-07-10 15:18:05 -0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,9 +4,9 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=uvite
+kernel.string=Void-R1
 kernel.compiler=AOSP clang
-kernel.made=CHRISL7
+kernel.made=Knotzy07x
 message.word=
 do.devicecheck=1
 do.modules=0
